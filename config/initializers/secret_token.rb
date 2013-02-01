25fa76366a542269a0ac38606318e0f8d1959aff
Sergii Ponomarov <sergii.ponomarov@gmail.com>
2013-02-01 19:55:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storex::Application.config.secret_token = '395a03e4f4af3cc014821deff2b748a39783e3af3f94c48a78ba718551e7c27af53ad792ae5a68fbed579fcd952c47ae5b15821b52a08712e5eae0d2c62b601c'
